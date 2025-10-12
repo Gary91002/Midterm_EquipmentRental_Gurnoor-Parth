05f4cb298e98b0a97234de3a4199938542dc097f
Parth Gajjar <147626010+ghostp13409@users.noreply.github.com>
2025-10-12 15:07:48 -0400

    Init AppDbConext

--- Backend/Midterm_EquipmentRental_Gurnoor&Parth/Data/AppDbContext.cs
+++ Backend/Midterm_EquipmentRental_Gurnoor&Parth/Data/AppDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace Midterm_EquipmentRental_Gurnoor_Parth.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Midterm_EquipmentRental_Gurnoor_Parth.Models;
+
+namespace Midterm_EquipmentRental_Gurnoor_Parth.Data
 {
-	public class AppDbContext
+	public class AppDbContext: DbContext
 	{
-	}
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+        // DbSets
+
+		// Auth DbSets
+		public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			base.OnModelCreating(modelBuilder);
+
+            // Seed data
+
+			modelBuilder.Entity<User>().HasData(
+				new User { Id = 1, Username = "admin", Password = "admin", Role = "Admin" },
+				new User { Id = 2, Username = "user", Password = "user", Role = "User" }
+			);
+        }
+
+    }
 }
---
+++ Backend/Midterm_EquipmentRental_Gurnoor&Parth/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Midterm_EquipmentRental_Gurnoor_Parth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
